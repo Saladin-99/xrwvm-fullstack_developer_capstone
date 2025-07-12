b683454e4d1e5577ba00f33aa2dda0a66b3609f3
Saladin-99 <salahabdou99@gmail.com>
2025-07-12 06:49:24 -0400

    Module 3b

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     # path for dealer reviews view
 
     # path for add a review view
